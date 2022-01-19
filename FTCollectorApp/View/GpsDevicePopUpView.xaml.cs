4dd229616d2c929fe360dfca7530617717d5658e
labdevsrc <frederickwong1995@gmail.com>
2022-01-19 11:03:22 +0700

    gps pop up crashed

--- FTCollectorApp/View/GpsDevicePopUpView.xaml.cs
+++ FTCollectorApp/View/GpsDevicePopUpView.xaml.cs
@@ -54,42 +54,47 @@ namespace FTCollectorApp.View
 
         private async void DeviceChecked(object sender, CheckedChangedEventArgs e)
         {
-            if (LocationService.Coords == null)
+            _location = LocationService.Coords;
+            if (_location == null)
             {
                 await LocationService.GetLocation();
-                _location = LocationService.Coords;
-                if (LocationService.Coords != null)
-                {
-                    var accuracy = String.Format("{0:0.###} m", _location.Accuracy.ToString());
-                    var lattitude = String.Format("{0:0.#######}", _location.Latitude.ToString());
-                    var longitude = String.Format("{0:0.#######}", _location.Longitude.ToString());
-                    txtAccuracy.Text = $"Accuracy is {accuracy}";
-                    txtCoords.Text = $"Current Point is {lattitude} ,{longitude} ";
-                    Session.gps_sts = "1";
-                    Session.lattitude2 = lattitude;
-                    Session.longitude2 = longitude;
-                }
+                
+
+            }
+
+            if (_location != null)
+            {
+                var accuracy = String.Format("{0:0.###} m", _location.Accuracy.ToString());
+                var lattitude = String.Format("{0:0.#######}", _location.Latitude.ToString());
+                var longitude = String.Format("{0:0.#######}", _location.Longitude.ToString());
+                txtAccuracy.Text = $"Accuracy is {accuracy}";
+                txtCoords.Text = $"Current Point is {lattitude} ,{longitude} ";
+                Session.gps_sts = "1";
+                Session.lattitude2 = lattitude;
+                Session.longitude2 = longitude;
             }
         }
 
         private async void ExternalChecked(object sender, CheckedChangedEventArgs e)
         {
-            
-            if (LocationService.Coords == null)
+            _location = LocationService.Coords;
+            if (_location == null)
             {
                 await LocationService.GetLocation();
-                _location = LocationService.Coords;
-                if (LocationService.Coords != null)
-                {
-                    var accuracy = String.Format("{0:0.###} m", _location.Accuracy.ToString());
-                    var lattitude = String.Format("{0:0.#######}", _location.Latitude.ToString());
-                    var longitude = String.Format("{0:0.#######}", _location.Longitude.ToString());
-                    txtAccuracy.Text = $"Accuracy is {accuracy}";
-                    txtCoords.Text = $"Current Point is {lattitude} ,{longitude} ";
-                    Session.gps_sts = "1";
-                    Session.lattitude2 = lattitude;
-                    Session.longitude2 = longitude;
-                }
+
+
+            }
+
+            if (_location != null)
+            {
+                var accuracy = String.Format("{0:0.###} m", _location.Accuracy.ToString());
+                var lattitude = String.Format("{0:0.#######}", _location.Latitude.ToString());
+                var longitude = String.Format("{0:0.#######}", _location.Longitude.ToString());
+                txtAccuracy.Text = $"Accuracy is {accuracy}";
+                txtCoords.Text = $"Current Point is {lattitude} ,{longitude} ";
+                Session.gps_sts = "1";
+                Session.lattitude2 = lattitude;
+                Session.longitude2 = longitude;
             }
         }
 
